81485d80f633f0ee5a95be75e3722ab9b3c41678
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-02 10:46:56 -0300

    added agenda

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles.css';
 import Sidebar from '../../components/Sidebar';
+import Agenda from '../../components/Agenda';
 import { Typography } from '@mui/material';
 
 export default function Home() {
   return(
     <div className="home">
       <Sidebar />
+      <Agenda />
       <Typography>yooo home!!!</Typography>
     </div>
   );
