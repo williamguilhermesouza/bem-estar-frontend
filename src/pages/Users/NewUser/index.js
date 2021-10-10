b533ae6fa5debb8b898d19d38ed3eb211a0fc77b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-10 17:37:50 -0300

    using container and paper

--- src/pages/Users/NewUser/index.js
+++ src/pages/Users/NewUser/index.js
@@ -1,4 +1,4 @@
-import { FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { FormControl, Grid, InputLabel, MenuItem, Paper, Select, TextField, Container } from '@mui/material';
 import React from 'react';
 import Sidebar from '../../../components/Sidebar';
 
@@ -14,45 +14,52 @@ export default function NewUser() {
         
         <div className="new-user">
             <Sidebar />
-            <p className="new-user-title">Novo Usuário</p>
-            <Grid container spacing={2}>
-                <Grid item xs={1}>
-                    <TextField fullWidth className="new-text-field" label="CPF"/>
-                </Grid>
-                <Grid item xs={3}>
-                    <TextField fullWidth className="new-text-field" label="Nome Completo" />
-                </Grid>
-                <Grid item xs={1}>
-                    <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                        <InputLabel id="demo-simple-select-standard-label">Sexo</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-standard-label"
-                            id="demo-simple-select-standard"
-                            value={sex}
-                            onChange={handleChange}
-                            label="Sexo"
-                        >
-                            <MenuItem value="Masculino">Masculino</MenuItem>
-                            <MenuItem value="Feminino">Feminino</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={1}>
-                    <TextField fullWidth className="new-text-field" label="Telefone" />
-                </Grid>
-                <Grid item xs={3}>
-                    <TextField fullWidth className="new-text-field" label="Rua" />
-                </Grid>
-                <Grid item xs={1}>
-                    <TextField fullWidth className="new-text-field" label="Bairro" />
-                </Grid>
-                <Grid item xs={1}>
-                    <TextField fullWidth className="new-text-field" label="Cidade" />
-                </Grid>
-                <Grid item xs={1}>
-                    <TextField fullWidth className="new-text-field" label="Estado" />
-                </Grid>
-            </Grid>
+            <Container maxWidth="xl">
+                <p className="new-user-title">Novo Usuário</p>
+                <Paper >
+                    <Grid container spacing={2}>
+                        <Grid item xs={2}>
+                            <TextField fullWidth className="new-text-field" label="CPF"/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <TextField fullWidth className="new-text-field" label="Nome Completo" />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                                <InputLabel id="demo-simple-select-standard-label">Sexo</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-standard-label"
+                                    id="demo-simple-select-standard"
+                                    value={sex}
+                                    onChange={handleChange}
+                                    label="Sexo"
+                                >
+                                    <MenuItem value="Masculino">Masculino</MenuItem>
+                                    <MenuItem value="Feminino">Feminino</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <TextField fullWidth className="new-text-field" label="Telefone" />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <TextField fullWidth className="new-text-field" label="Rua" />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField fullWidth className="new-text-field" label="Número" />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField fullWidth className="new-text-field" label="Bairro" />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField fullWidth className="new-text-field" label="Cidade" />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <TextField fullWidth className="new-text-field" label="Estado" />
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Container>
         </div>
     );
 }
