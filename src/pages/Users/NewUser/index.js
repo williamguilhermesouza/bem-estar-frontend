a2bfa1fa6c5e14bec1da07af87c905fcad43358e
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-10 13:52:44 -0300

    creating form page

--- src/pages/Users/NewUser/index.js
+++ src/pages/Users/NewUser/index.js
@@ -1,21 +1,57 @@
-import { Grid, TextField } from '@mui/material';
+import { FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import React from 'react';
 import Sidebar from '../../../components/Sidebar';
 
 export default function NewUser() {
+    const [sex, setSex] = React.useState('');
+
+    const handleChange = (event) => {
+        setSex(event.target.value);
+    };
+
+
     return(
         
         <div className="new-user">
             <Sidebar />
             <p className="new-user-title">Novo Usuário</p>
             <Grid container spacing={2}>
-                <Grid item xs={3}>
+                <Grid item xs={1}>
                     <TextField fullWidth className="new-text-field" label="CPF"/>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={3}>
                     <TextField fullWidth className="new-text-field" label="Nome Completo" />
                 </Grid>
-                
+                <Grid item xs={1}>
+                    <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-standard-label">Sexo</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-standard-label"
+                            id="demo-simple-select-standard"
+                            value={sex}
+                            onChange={handleChange}
+                            label="Sexo"
+                        >
+                            <MenuItem value="Masculino">Masculino</MenuItem>
+                            <MenuItem value="Feminino">Feminino</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={1}>
+                    <TextField fullWidth className="new-text-field" label="Telefone" />
+                </Grid>
+                <Grid item xs={3}>
+                    <TextField fullWidth className="new-text-field" label="Rua" />
+                </Grid>
+                <Grid item xs={1}>
+                    <TextField fullWidth className="new-text-field" label="Bairro" />
+                </Grid>
+                <Grid item xs={1}>
+                    <TextField fullWidth className="new-text-field" label="Cidade" />
+                </Grid>
+                <Grid item xs={1}>
+                    <TextField fullWidth className="new-text-field" label="Estado" />
+                </Grid>
             </Grid>
         </div>
     );
