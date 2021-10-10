c4c0a8a40ee07efd594ae27f5b91ff210782d559
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-09 22:34:07 -0300

    working on new users page

--- src/pages/Users/NewUser/index.js
+++ src/pages/Users/NewUser/index.js
@@ -1,9 +1,22 @@
+import { Grid, TextField } from '@mui/material';
 import React from 'react';
+import Sidebar from '../../../components/Sidebar';
 
 export default function NewUser() {
     return(
+        
         <div className="new-user">
-            <p>new user</p>
+            <Sidebar />
+            <p className="new-user-title">Novo Usuário</p>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <TextField fullWidth className="new-text-field" label="CPF"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth className="new-text-field" label="Nome Completo" />
+                </Grid>
+                
+            </Grid>
         </div>
     );
 }
