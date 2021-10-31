1efaa033b44788e4bc793eb63f61d849b5ee2b53
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-31 16:29:44 -0300

    working on movements page

--- src/pages/Movements/index.js
+++ src/pages/Movements/index.js
@@ -30,27 +30,39 @@ export default function Movements() {
   const [originalMovements, setOriginalMovements] = useState([]); 
   const [filterField, setFilterField] = useState('');
 
+  
   useEffect(() => {
-    getMovements().then( 
-      response => {
-        
-        let movs = [];
-        response.data.forEach(async element => {
-          const patient = element.patientId? await getPatient(element.patientId): {name: ''};
-          movs = [...movs, {
-            id: element.id,
-            patientName: patient.name,
-            description: element.description,
-            value: element.value,
-          }]
-        });
+    
+    const fetchData = async () => {
+      const response = await getMovements();
+      const data = response.data;
+      let movs = [];
+
+      data.forEach(async element => {
+        let patient;
+
+        if (element.patientId) {
+          patient = await getPatient(element.patientId);
+          patient = patient.data;
+        } else {
+          patient = {name: ''};
+        }
+
+        movs = [...movs, {
+          id: element.id,
+          patientName: patient.name,
+          description: element.description,
+          value: element.value,
+        }];
 
         setMovements(movs); 
         setOriginalMovements(movs);
-        console.log(movs);
-      }
-    )
-  },[setMovements]);
+      });
+
+
+    }
+    fetchData();
+  },[setMovements, setOriginalMovements]);
 
   const columns = [
     { field: 'id', headerName: 'ID', width: 130 },
@@ -69,10 +81,8 @@ export default function Movements() {
 
   function handleMovementFilter() {
     setMovements(
-
       originalMovements.filter(movement => {
-        // must be patientId === filterField
-        return movement.name.includes(filterField)
+        return movement.patientName.includes(filterField);
       })
     );
   }
@@ -86,7 +96,7 @@ export default function Movements() {
       <Sidebar />
       <Container maxWidth="xl">
         <TextField label="Nome do Paciente" className={classes.searchField} value={filterField} onChange={handleFieldChange} />
-        <Button variant="contained" className={classes.userButtons} sx={{marginTop: '5px', marginLeft: '15px'}} href="/movements/patient" onClick={handleMovementFilter}>Filtrar</Button>
+        <Button variant="contained" className={classes.userButtons} sx={{marginTop: '5px', marginLeft: '15px'}} onClick={handleMovementFilter}>Filtrar</Button>
       </Container>
         <DataGrid
           rows={movements}
