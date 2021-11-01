d84d01502e8206c1ae17ae0232cdf77756f55b89
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-31 21:34:53 -0300

    first works with charts on overview page

--- src/pages/Overview/index.js
+++ src/pages/Overview/index.js
@@ -1,13 +1,79 @@
 import React from 'react';
-
 import Sidebar from '../../components/Sidebar';
-import { Typography } from '@mui/material';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+
 
 export default function Overview() {
+
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
+
   return(
     <div className="overview">
       <Sidebar />
-      <Typography>yooo overview!!!</Typography>
+      <LineChart
+        width={1600}
+        height={700}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 400,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
     </div>
   );
 }
